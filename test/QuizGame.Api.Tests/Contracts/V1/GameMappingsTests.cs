67b695975882b258e3ab8cfe625277ab5875005f
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-10-25 19:40:10 +0100

    Added v1 contract mappings tests

---
+++ test/QuizGame.Api.Tests/Contracts/V1/GameMappingsTests.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using QuizGame.Api.Contracts.V1;
+using QuizGame.Domain.Entities;
+
+namespace QuizGame.Api.Tests.Contracts.V1;
+
+public class GameMappingsTests
+{
+    [Fact]
+    public void ToDomain_ShouldMapCreateRequestToDomain()
+    {
+        // Arrange
+        var request = new GameCreateRequest(Guid.NewGuid(), DateTime.UtcNow, Random.Shared.Next(0, 5));
+
+        // Act
+        var result = request.ToDomain();
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Id.Should().NotBeEmpty();
+        result.QuizId.Should().Be(request.QuizId);
+        result.Played.Should().Be(request.Played);
+        result.Score.Should().Be(request.Score);
+    }
+
+    [Fact]
+    public void ToDomain_ShouldMapUpdateRequestToDomain()
+    {
+        // Arrange
+        var originalEntity = new Game
+        {
+            Id = Guid.NewGuid(),
+            QuizId = Guid.NewGuid(),
+            Played = DateTime.UtcNow.AddHours(-1),
+            Score = Random.Shared.Next(0, 5),
+        };
+
+        var updateRequest = new GameUpdateRequest(DateTime.UtcNow, Random.Shared.Next(0, 5));
+
+        // Act
+        var result = updateRequest.ToDomain(originalEntity);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Id.Should().Be(originalEntity.Id);
+        result.QuizId.Should().Be(originalEntity.QuizId);
+        result.Played.Should().Be(updateRequest.Played);
+        result.Score.Should().Be(updateRequest.Score);
+    }
+
+    [Fact]
+    public void ToResponse_ShouldMapDomainToResponse()
+    {
+        // Arrange
+        var domainEntity = new Game
+        {
+            Id = Guid.NewGuid(),
+            QuizId = Guid.NewGuid(),
+            Played = DateTime.UtcNow,
+            Score = Random.Shared.Next(0, 5),
+        };
+
+        // Act
+        var result = domainEntity.ToResponse();
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Id.Should().Be(domainEntity.Id);
+        result.QuizId.Should().Be(domainEntity.QuizId);
+        result.Played.Should().Be(domainEntity.Played);
+        result.Score.Should().Be(domainEntity.Score);
+    }
+}
---
+++ test/QuizGame.Api.Tests/Contracts/V1/QuizMappingsTests.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using QuizGame.Api.Contracts.V1;
+using QuizGame.Domain.Entities;
+
+namespace QuizGame.Api.Tests.Contracts.V1;
+
+public class QuizMappingsTests
+{
+    [Fact]
+    public void ToDomain_ShouldMapCreateRequestToDomain()
+    {
+        // Arrange
+        var request = new QuizCreateRequest("Sample Name", "Sample Description");
+
+        // Act
+        var result = request.ToDomain();
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Id.Should().NotBeEmpty();
+        result.Name.Should().Be(request.Name);
+        result.Description.Should().Be(request.Description);
+    }
+
+    [Fact]
+    public void ToDomain_ShouldMapUpdateRequestToDomain()
+    {
+        // Arrange
+        var originalEntity = new Quiz
+        {
+            Id = Guid.NewGuid(),
+            Name = "Original Name",
+            Description = "Original Description",
+        };
+
+        var updateRequest = new QuizUpdateRequest("Updated Name", "Updated Description");
+
+        // Act
+        var result = updateRequest.ToDomain(originalEntity);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Id.Should().Be(originalEntity.Id);
+        result.Name.Should().Be(updateRequest.Name);
+        result.Description.Should().Be(updateRequest.Description);
+    }
+
+    [Fact]
+    public void ToResponse_ShouldMapDomainToResponse()
+    {
+        // Arrange
+        var domainEntity = new Quiz
+        {
+            Id = Guid.NewGuid(),
+            Name = "Quiz Name",
+            Description = "Quiz Description"
+        };
+
+        // Act
+        var result = domainEntity.ToResponse();
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Id.Should().Be(domainEntity.Id);
+        result.Name.Should().Be(domainEntity.Name);
+        result.Description.Should().Be(domainEntity.Description);
+    }
+}
