2f970990dc4e002410de3d41f275bcbc601a5c13
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-11-06 20:14:54 +0000

    Implemented games table with pagination and sortation

--- src/QuizGame.Web/src/app/games/games.component.ts
+++ src/QuizGame.Web/src/app/games/games.component.ts
@@ -1,69 +1,72 @@
 import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, inject, ViewChild } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { QuizGameService } from '../shared/quiz-game.service';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatSort, MatSortModule } from '@angular/material/sort';
-import { Game } from '../shared/game.interface';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { catchError, map, merge, of, startWith, switchMap } from 'rxjs';
+import { Game } from '../shared/game.interface';
+import { QuizGameService } from '../shared/quiz-game.service';
 import { PaginatedGames } from '../shared/paginated-game.interface';
 import { ErrorComponent } from '../error/error.component';
 
 @Component({
   selector: 'app-games',
   standalone: true,
-  imports: [CommonModule, ErrorComponent, MatPaginatorModule, MatProgressSpinnerModule, MatSortModule],
+  imports: [CommonModule, ErrorComponent, MatPaginatorModule, MatProgressSpinnerModule, MatSortModule, MatTableModule],
   templateUrl: './games.component.html',
   styleUrl: './games.component.css'
 })
 export class GamesComponent implements AfterViewInit {
   games: Game[] = [];
   totalRecords: number = 0;
-  tableColumns: string[] = ['id', 'quizid', 'played', 'score']
+  tableColumns: string[] = ['id', 'quizId', 'played', 'score']
   isLoading: boolean = true;
   isError: boolean = false;
   errorMessage: string = '';
+  pageIndex: number = 0;
+  pageSize: number = 10;
+  sortBy: string = '';
   
   quizGameService = inject(QuizGameService);
   
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-  //@ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatSort) sort!: MatSort;
 
   ngAfterViewInit(): void {
-    this.loadGames()
-
-    //this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
-    
-    //merge(this.sort.sortChange, this.paginator.page)
-    //merge(this.paginator.page)
-    //.pipe(
-    //   startWith({}),
-    //   switchMap(() => {
-    //     this.isLoading = true;
-    //     return this.quizGameService.getGames().pipe(catchError(() => of(null)));
-    //   }),
-    //   map(data => {
-    //     this.isLoading = false;
-    //     if (data === null) {
-    //       return [];
-    //     }
-
-    //     this.totalRecords = data.totalRecords;
-    //     return data.games;
-    //   }),
-    // )
-    // .subscribe(data => (this.games = data));
+    merge(this.sort.sortChange, this.paginator.page)
+    .pipe(
+      startWith({}),
+      switchMap(() => {
+        this.isLoading = true;
+        return this.quizGameService.getGames(this.pageIndex, this.pageSize, this.sortBy).pipe(catchError(() => of(null)));
+      }),
+      map(data => {
+        this.isLoading = false;
+        if (data === null) {
+          return [];
+        }
+        
+        this.totalRecords = data.totalRecords;
+        return data.games;
+      }),
+    )
+    .subscribe(data => (this.games = data));    
   }
 
-  private loadGames(): void {
-    this.quizGameService.getGames().subscribe((response: PaginatedGames) => {
-      this.totalRecords = response.totalRecords;
-      this.games = response.games;
-    }, error => {
-      console.log("error", error);
-      this.isLoading = false;
-      this.isError = true;
-      this.errorMessage = error.message;
-    })
+  handlePageEvent(e: PageEvent) {
+    this.totalRecords = e.length;
+    this.pageIndex = e.pageIndex;
+    this.pageSize = e.pageSize;
+  }
+  
+  handleSortEvent(e: Sort) {
+    if (!this.sort.active || this.sort.direction === '') {
+      this.sortBy = '';
+    }
+    else {
+      this.sortBy = `${e.active}-${e.direction}`;
+    }   
+    this.pageIndex = 0;
   }
 }
--- src/QuizGame.Web/src/app/shared/quiz-game.service.ts
+++ src/QuizGame.Web/src/app/shared/quiz-game.service.ts
@@ -7,14 +7,14 @@ import { PaginatedGames } from './paginated-game.interface';
   providedIn: 'root'
 })
 export class QuizGameService {
-  private url = 'https://localhost:7083/api/v1/quizgame';
+  private url = 'http://localhost:5283/api/v1/quizgame';
   private httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
   
   constructor(private http: HttpClient) { }
   
-  getGames(): Observable<PaginatedGames> {
-    return this.http.get<PaginatedGames>(`${this.url}/games`);
+  getGames(pageIndex: number, pageSize: number, sortBy: string): Observable<PaginatedGames> {
+    return this.http.get<PaginatedGames>(`${this.url}/games/page?sortBy=${sortBy}&page=${pageIndex + 1}&size=${pageSize}`);
   }
 }
