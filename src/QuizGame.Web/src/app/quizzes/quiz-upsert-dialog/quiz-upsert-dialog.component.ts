30081edd68fa3664d502388fa06277eb5af63ca3
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-11-11 14:02:38 +0000

    Added form fields to dialog

--- src/QuizGame.Web/src/app/quizzes/quiz-upsert-dialog/quiz-upsert-dialog.component.ts
+++ src/QuizGame.Web/src/app/quizzes/quiz-upsert-dialog/quiz-upsert-dialog.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {
   MatDialogModule,
@@ -9,31 +9,44 @@ import {
   MatDialogRef,
 } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatStepperModule } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-quiz-upsert-dialog',
   standalone: true,
   imports: [
+    CommonModule,
     FormsModule,
     MatFormFieldModule,
     MatButtonModule,
     MatDialogContent,
     MatDialogModule,
     MatDialogTitle,
+    MatIconModule,
+    MatInputModule,
     MatStepperModule,
     ReactiveFormsModule,
   ],
   templateUrl: './quiz-upsert-dialog.component.html',
   styleUrl: './quiz-upsert-dialog.component.css',
 })
-export class QuizUpsertDialogComponent {
+export class QuizUpsertDialogComponent implements OnInit {
+  quizForm!: FormGroup;
+  
   readonly dialogRef = inject(MatDialogRef<QuizUpsertDialogComponent>);
   private formBuilder = inject(FormBuilder);
-
-  firstFormGroup = this.formBuilder.group({
-    firstCtrl: '',
-  });
+  
+  ngOnInit(): void {
+    this.quizForm = this.formBuilder.group(
+      {
+        name: ['', Validators.required],
+        description: '',
+      }
+    );
+  }
+    
   secondFormGroup = this.formBuilder.group({
     secondCtrl: '',
   });
