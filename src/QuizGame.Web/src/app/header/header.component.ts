3c6a70c0317af7f62ed7d5244b2796f8e289624c
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-11-19 11:59:17 +0000

    Updated header to use icons for buttons and tooltips for text

--- src/QuizGame.Web/src/app/header/header.component.ts
+++ src/QuizGame.Web/src/app/header/header.component.ts
@@ -3,14 +3,19 @@ import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    RouterLink,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
