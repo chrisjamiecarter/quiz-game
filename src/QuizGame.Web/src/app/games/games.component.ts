2b063dfdedf4e6ef5ce6c9dfc0b76487601cdfd8
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-11-21 19:38:10 +0000

    Implemented game filtering

--- src/QuizGame.Web/src/app/games/games.component.ts
+++ src/QuizGame.Web/src/app/games/games.component.ts
@@ -1,17 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, inject, ViewChild } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import {
   MatPaginator,
   MatPaginatorModule,
   PageEvent,
 } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelect, MatSelectModule } from '@angular/material/select';
 import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
-import { catchError, map, merge, of, startWith, switchMap } from 'rxjs';
+import {
+  catchError,
+  map,
+  merge,
+  of,
+  startWith,
+  Subject,
+  switchMap,
+} from 'rxjs';
 import { Game } from '../shared/game.interface';
 import { QuizGameService } from '../shared/quiz-game.service';
 import { ErrorComponent } from '../error/error.component';
+import { Quiz } from '../shared/quiz.interface';
 
 @Component({
   selector: 'app-games',
@@ -19,18 +34,29 @@ import { ErrorComponent } from '../error/error.component';
   imports: [
     CommonModule,
     ErrorComponent,
+    FormsModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatPaginatorModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
     MatSortModule,
     MatTableModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './games.component.html',
   styleUrl: './games.component.css',
 })
 export class GamesComponent implements AfterViewInit {
   games: Game[] = [];
+  quizzes: Quiz[] = [];
   totalRecords: number = 0;
   tableColumns: string[] = ['played', 'quiz', 'score'];
+  tableFilterFromDate: Date | null = null;
+  tableFilterToDate: Date | null = null;
+  tableFilterQuizId: string | null = null;
   isLoading: boolean = true;
   isError: boolean = false;
   errorMessage: string = '';
@@ -38,19 +64,34 @@ export class GamesComponent implements AfterViewInit {
   pageSize: number = 10;
   sortBy: string = '';
 
+  dateChanged: Subject<void> = new Subject<void>();
+
   quizGameService = inject(QuizGameService);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSelect) select!: MatSelect;
   @ViewChild(MatSort) sort!: MatSort;
 
   ngAfterViewInit(): void {
-    merge(this.sort.sortChange, this.paginator.page)
+    merge(
+      this.dateChanged.asObservable(),
+      this.paginator.page,
+      this.select.selectionChange,
+      this.sort.sortChange
+    )
       .pipe(
         startWith({}),
         switchMap(() => {
           this.isLoading = true;
           return this.quizGameService
-            .getGames(this.pageIndex, this.pageSize, this.sortBy)
+            .getGames(
+              this.tableFilterQuizId,
+              this.tableFilterFromDate,
+              this.tableFilterToDate,
+              this.pageIndex,
+              this.pageSize,
+              this.sortBy
+            )
             .pipe(
               catchError((error) => {
                 this.isError = true;
@@ -70,10 +111,30 @@ export class GamesComponent implements AfterViewInit {
         })
       )
       .subscribe((data) => (this.games = data));
+
+    this.loadQuizzes();
   }
 
   getScorePercentage(score: number, maxScore: number): number {
     return Math.floor((100 * score) / maxScore);
+  }
+
+  loadQuizzes(): void {
+    this.isLoading = true;
+    this.quizGameService.getQuizzes().subscribe({
+      next: (quizzes) => {
+        this.isLoading = false;
+        this.quizzes = quizzes;
+      },
+      error: (error) => {
+        this.isLoading = false;
+        this.isError = true;
+        this.errorMessage = error;
+      },
+      complete: () => {
+        this.isLoading = false;
+      },
+    });
   }
 
   handlePageEvent(e: PageEvent) {
@@ -89,5 +150,22 @@ export class GamesComponent implements AfterViewInit {
       this.sortBy = `${e.active}-${e.direction}`;
     }
     this.pageIndex = 0;
+  }
+
+  onFilterDateChanged(): void {
+    this.pageIndex = 0;
+    this.dateChanged.next();
+  }
+
+  onFilterSelectionChanged(): void {
+    this.pageIndex = 0;
+  }
+
+  onResetFilter(): void {
+    this.pageIndex = 0;
+    this.tableFilterFromDate = null;
+    this.tableFilterToDate = null;
+    this.tableFilterQuizId = null;
+    this.dateChanged.next();
   }
 }
